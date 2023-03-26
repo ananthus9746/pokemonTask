99272ecbbfa115477692c297fc12f99360f781d1
ananthus9746 <ananthus.ann@gmail.com>
2023-03-26 07:22:00 +0530

    inifity scroll scroll completed

--- client/src/pages/HomePage/Home.js
+++ client/src/pages/HomePage/Home.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useRef } from "react";
-import { getPokemon } from "../../APIs/Api";
 import PokemonCard from "../../components/PokemonCard/PokemonCard";
 import "./Home.css";
 import axios from 'axios'
@@ -9,16 +8,11 @@ import axios from 'axios'
 function Home() {
 
   const [records, setRecords] = useState([]);
-  const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const loaderRef = useRef(null);
-
-
-  const [data, setData] = useState([])
   const [offset, setOffset] = useState(0)
-  const [limit,setLimit]=useState(3)
+  const [limit, setLimit] = useState(3)
   const [nodData, setnoData] = useState(false)
 
   useEffect(() => {
@@ -29,15 +23,11 @@ function Home() {
         // const { data, totalPages } = response.data;
         console.log("response.data..", response.data)
         var data = response.data.getedPartners
-        var totalPages = response.data.totalPages
 
         console.log("data..", data)
-
-
         if (data.length < 1) {
           setnoData(true)
         }
-
         setRecords(prevRecords => [...prevRecords, ...data]);
         // setTotalPages(totalPages);
 
@@ -50,22 +40,14 @@ function Home() {
     fetchRecords();
   }, [offset]);
 
-
-
   useEffect(() => {
-
-
     if (!nodData) {
-      
       const handleScroll = (e) => {
         const scrollHeight = e.target.documentElement.scrollHeight
         const currentHeight = e.target.documentElement.scrollTop + window.innerHeight
         if (currentHeight + 1 >= scrollHeight)
           setOffset(offset + 1)
       }
-
-      { data.length > 1 ? console.log("data..here..") : console.log("no data..") }
-
       window.addEventListener("scroll", handleScroll)
       return () => window.removeEventListener("scroll", handleScroll)
     }
@@ -74,24 +56,18 @@ function Home() {
 
 
 
-
-
-
-
-
-
-
-
   return (
+
     <div className="Home_Wrapper">
+     {error?<>Erorr:{error}</>:""}
       <div className="Card_container">
         {records.map((obj) =>
           <PokemonCard obj={obj} />
         )}
 
       </div>
-
-      <div ref={loaderRef}>{loading && 'Loading...'}</div>
+      <div className="loading" ref={loaderRef}>{loading && 'Loading...'}</div>
+      {nodData?<p className="loading">Completed all datas are viewed</p>:""}
 
     </div>
   );
